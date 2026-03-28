4530590d1a8c611b7f6057daa2f6cb2ecf424afc
ayrton <ayrton_ito@hotmail.com>
2026-03-28 15:18:40 -0300

    browser update

---
+++ Windows/Browser/user.js
@@ -0,0 +1,7 @@
+user_pref("zen.theme.content-element-separation", 0);
+user_pref("zen.workspaces.show-workspace-indicator", false);
+user_pref("zen.urlbar.replace-newtab", false);
+user_pref("browser.tabs.closeWindowWithLastTab", true);
+user_pref("zen.view.show-clear-tabs-button", false);
+user_pref("zen.view.compact.show-background-tab-toast", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
